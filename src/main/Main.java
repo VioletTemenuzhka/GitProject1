1ef12218bc5ab72fa1ac3627d1677f001bf7c28b
VioletTemenuzhka <violet.chan.fl@gmail.com>
2022-05-17 00:15:49 -0400

    updated commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,10 +1,11 @@
 package main;
 
-public class Test {
+public class Main {
     public static void main(String[] args) {
         System.out.println("I am Groot!!");
         System.out.println("I am correct code");
 
         System.out.println("Very confused");
+        System.out.println("My updated commit");
     }
 }
